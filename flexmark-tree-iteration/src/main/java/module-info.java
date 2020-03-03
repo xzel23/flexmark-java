0c40a76a428f178776b9530c609eed086fd2100c
Axel Howind <axel@dua3.com>
2020-03-03 23:02:21 +0100

    replace slf4j by JPMS enabled version; cleanup

--- flexmark-tree-iteration/src/main/java/module-info.java
+++ flexmark-tree-iteration/src/main/java/module-info.java
@@ -1,8 +1,7 @@
 module flexmark.tree.iteration {
     requires org.jetbrains.annotations;
-    requires 
-flexmark.util.data;
-    requires slf4j.api;
+    requires flexmark.util.data;
+    requires org.slf4j;
     requires flexmark.util.ast;
     requires flexmark.util.collection;
     exports com.vladsch.flexmark.tree.iteration;
